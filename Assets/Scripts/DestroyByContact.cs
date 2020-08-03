facedd73be53990eedc9c9955a12b23ffbea7b4d
carlosmu <carlos.damian.munoz@gmail.com>
2020-08-03 11:56:37 -0300

    Space Shooter primera parte.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary")) return;
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        if(other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+    private Rigidbody rig;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+         rig.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+}
