5eabd3f60a37fcaf6b6deb54d227bbefd49414f4
carlosmu <carlos.damian.munoz@gmail.com>
2020-08-03 17:21:26 -0300

    Puntaje, restart y Game Over

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,15 +6,32 @@ public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
+
+    public int scoreValue;
+    public GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        gameController = gameControllerObject.GetComponent<GameController>();
+         
+        /* 
+         * Misma funcionalidad, código reducido:
+         * gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+         */
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Boundary")) return;
 
+        // Cuando el player es destruido y se disparan la explosión y el "GameOver"
         Instantiate(explosion, transform.position, transform.rotation);
         if(other.CompareTag("Player"))
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
         }
+        gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,28 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
-    public Vector3 spawnValues;
-    public int hazardCount;
-    public float spawnWait;
-    public float startWait;
-    public float waveWait;
+    public GameObject hazard; // Daño
+    public Vector3 spawnValues; // Posición de Instanciación de asteroides
+    public int hazardCount; // Cantidad de asteroides
+    public float spawnWait; // Tiempo de espera de instanciación de asteroides (por cada uno de ellos)
+    public float startWait; // Tiempo de espera inicial para instanciar asteroides
+    public float waveWait; // Tiempo de espera de instanciación de asteroides (por olas)
+
+    // Variables para la puntuación
+    private int score;
+    public Text scoreText;
+
+    //Variables para gameOver y restart
+    public Text restartText;
+    public Text gameOverText;
+    
+    private bool restart;
+    private bool gameOver;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Restart en false y ocultar su texto de nueva partida
+        restart = false;
+        restartText.gameObject.SetActive(false);
+        // Game Over en false y ocultar su texto
+        gameOver = false;
+        gameOverText.gameObject.SetActive(false);
+        // Poner el Score en 0 y Actualizar score
+        score = 0;
+        UpdateScore();
+        // Corrutina de Instanciar Olas de Asteroides
         StartCoroutine(SpawnWaves());        
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+        if(restart && Input.GetKeyDown(KeyCode.R))
+        {
+            // Recarga la escena en base a la escena activa.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        }
+    }
+
+    // Instanciar holas de asterioide mientras las condiciones se cumplan
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
         while (true)
         {
+            // Generar olas de asteroides
             for (int i=0; i<hazardCount; i++)
             {
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
@@ -30,6 +63,34 @@ public class GameController : MonoBehaviour
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
+
+            // Si hemos perdido, mostrar el texto para una nueva partida
+            if (gameOver)
+            {
+                restartText.gameObject.SetActive(true);
+                restart = true;
+                break;
+            }
         }
     }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScore();
+    }
+
+    // Actualizar el score
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    // Game over
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        gameOver = true;
+    }
+
 }
