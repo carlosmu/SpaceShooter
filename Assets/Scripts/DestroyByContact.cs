0de728e3b1120ee9fa5d79848d06d2216594d172
carlosmu <carlos.damian.munoz@gmail.com>
2020-08-04 19:04:22 -0300

    Agrega disparos a los enemigos

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -22,15 +22,24 @@ public class DestroyByContact : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Boundary")) return;
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy")) return;
 
         // Cuando el player es destruido y se disparan la explosión y el "GameOver"
-        Instantiate(explosion, transform.position, transform.rotation);
+        
+        // Si explosion (en el inspector) es distinto de null...
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        
+        // Si impacta con objetos que tienen el tag "Player"...
         if(other.CompareTag("Player"))
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
             gameController.GameOver();
         }
+
+        // Agregar puntaje
         gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
---
+++ Assets/Scripts/EnemyWeaponController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeaponController : MonoBehaviour
+{
+    public GameObject shot;
+    public Transform shotSpawn;
+    // Retardo y tasa de disparos del enemigo.
+    public float delay;
+    public float fireRate;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Disparos del enemigo
+        InvokeRepeating("Fire", delay, fireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
+    }
+}
